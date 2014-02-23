e387bbaf1539902cb9ee039c07a9c2ba78812f34
Razvan <radunesiu@yahoo.com>
2014-02-22 17:14:05 -0800

    the good stuff

---
+++ codewars/src/main/java/com/bluescreen/codewars/Amibae.java
@@ -0,0 +1,39 @@
+package com.bluescreen.codewars;
+
+import android.graphics.Color;
+import android.media.Image;
+
+
+/**
+ * Created by Radu & Razvan Nesiu on 22/02/14.
+ */
+public class Amibae extends Sprite {
+
+
+
+public Amibae(int x, int y,Color color,int size)
+{
+    super(x,y,color,size);
+}
+
+    public Amibae(int x, int y,Color color,int size,int dx,int dy)
+    {
+        super(x,y,color,size,dx,dy);
+    }
+
+
+@Override
+public void draw()
+{
+
+
+}
+
+
+
+
+
+
+
+
+}
